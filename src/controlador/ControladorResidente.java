617454a074511ee27c961ac9f35051a9c2a2aa79
Usuario <Usuario@DESKTOP-04R5SE0>
2018-10-18 13:44:26 -0500

    otro controler

--- src/controlador/ControladorResidente.java
+++ src/controlador/ControladorResidente.java
@@ -1,5 +1,33 @@
 package controlador;
 
-public class ControladorResidente {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import vista.VistaResidentes;
+
+public class ControladorResidente implements ActionListener {
+	
+	private final VistaResidentes vr;
+	
+	public ControladorResidente(VistaResidentes vr) {
+		this.vr=vr;
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent llamar) {
+		switch (llamar.getActionCommand()) {
+		
+		case "guardar":
+			
+			
+			break;
+			
+		case""	
+		}
+		
+	}
+	
+	
 
 }
