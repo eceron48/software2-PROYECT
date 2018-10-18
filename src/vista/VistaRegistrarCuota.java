3794cfc61bef2acd4fffd22c9eb577ed52a776f1
ThinkPad <ThinkPad@JULIANAPCH>
2018-10-17 23:04:30 -0500

    Modificacion vista de registrar cuota

--- src/vista/VistaRegistrarCuota.java
+++ src/vista/VistaRegistrarCuota.java
@@ -41,58 +41,72 @@ public class VistaRegistrarCuota extends JFrame {
 	public VistaRegistrarCuota() {
 		setTitle("REGISTRAR CUOTA DE ADMINISTRACION");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 382, 228);
+		setBounds(100, 100, 371, 276);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(255, 255, 153));
+		contentPane.setBackground(new Color(153, 255, 153));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblValorCuota = new JLabel("Valor Cuota:");
-		lblValorCuota.setBounds(32, 26, 75, 14);
-		contentPane.add(lblValorCuota);
-		
-		txtValorCuota = new JTextField();
-		txtValorCuota.setBounds(108, 23, 86, 20);
-		contentPane.add(txtValorCuota);
-		txtValorCuota.setColumns(10);
-		
-		JLabel lblPorcentaje = new JLabel("Porcentaje:");
-		lblPorcentaje.setBounds(32, 54, 75, 14);
-		contentPane.add(lblPorcentaje);
-		
-		txtPorcentaje = new JTextField();
-		txtPorcentaje.setColumns(10);
-		txtPorcentaje.setBounds(108, 51, 86, 20);
-		contentPane.add(txtPorcentaje);
-		
-		JLabel lblTotal = new JLabel("TOTAL:");
-		lblTotal.setBounds(32, 85, 46, 14);
+		JLabel lblTotal = new JLabel("TOTAL A PAGAR:");
+		lblTotal.setBounds(32, 112, 101, 14);
 		contentPane.add(lblTotal);
 		
 		txtTotal = new JTextField();
-		txtTotal.setBounds(108, 82, 86, 21);
+		txtTotal.setBounds(137, 109, 186, 21);
 		contentPane.add(txtTotal);
 		txtTotal.setColumns(10);
 		
+		JPanel panel = new JPanel();
+		panel.setBounds(32, 21, 291, 80);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblValorCuota = new JLabel("Valor Cuota:");
+		lblValorCuota.setBounds(10, 11, 75, 14);
+		panel.add(lblValorCuota);
+		
+		JLabel lblPorcentaje = new JLabel("Porcentaje:");
+		lblPorcentaje.setBounds(10, 51, 75, 14);
+		panel.add(lblPorcentaje);
+		
+		txtPorcentaje = new JTextField();
+		txtPorcentaje.setBounds(95, 48, 86, 20);
+		panel.add(txtPorcentaje);
+		txtPorcentaje.setColumns(10);
+		
+		txtValorCuota = new JTextField();
+		txtValorCuota.setBounds(95, 8, 86, 20);
+		panel.add(txtValorCuota);
+		txtValorCuota.setColumns(10);
+		
 		JButton btnAgregar = new JButton("");
+		btnAgregar.setToolTipText("Agregar");
+		btnAgregar.setBounds(212, 11, 67, 57);
+		panel.add(btnAgregar);
 		btnAgregar.setIcon(new ImageIcon(VistaRegistrarCuota.class.getResource("/iconos/Crear.png")));
-		btnAgregar.setBounds(32, 121, 67, 57);
-		contentPane.add(btnAgregar);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(32, 141, 295, 72);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
 		
 		JButton btnGuardar = new JButton("");
+		btnGuardar.setToolTipText("Guardar");
+		btnGuardar.setBounds(218, 11, 67, 57);
+		panel_1.add(btnGuardar);
 		btnGuardar.setIcon(new ImageIcon(VistaRegistrarCuota.class.getResource("/iconos/Guardar.png")));
-		btnGuardar.setBounds(108, 121, 67, 57);
-		contentPane.add(btnGuardar);
 		
 		JButton btnModificar = new JButton("");
+		btnModificar.setToolTipText("Modificar");
+		btnModificar.setBounds(116, 11, 67, 57);
+		panel_1.add(btnModificar);
 		btnModificar.setIcon(new ImageIcon(VistaRegistrarCuota.class.getResource("/iconos/Modificar (2).png")));
-		btnModificar.setBounds(185, 121, 67, 57);
-		contentPane.add(btnModificar);
 		
 		JButton btnNewButton = new JButton("");
+		btnNewButton.setToolTipText("Ver Cuotas");
+		btnNewButton.setBounds(10, 11, 61, 57);
+		panel_1.add(btnNewButton);
 		btnNewButton.setIcon(new ImageIcon(VistaRegistrarCuota.class.getResource("/iconos/mostrar.png")));
-		btnNewButton.setBounds(262, 121, 61, 57);
-		contentPane.add(btnNewButton);
 	}
 }
