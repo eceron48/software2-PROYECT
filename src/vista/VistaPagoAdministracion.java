311a158006be4cbf0661a7941b4c1e1e07b1715d
ThinkPad <ThinkPad@JULIANAPCH>
2018-11-05 19:46:39 -0500

    Vista credito y arreglo pago admin

--- src/vista/VistaPagoAdministracion.java
+++ src/vista/VistaPagoAdministracion.java
@@ -31,15 +31,14 @@ public class VistaPagoAdministracion extends JFrame {
 	public JPanel contentPane;
 	public JTextField txtCedula;
 	public JTextField txtNombre;
-	public DefaultTableModel tbpersonas;
 	public JTextField txtTotal;
 	public JButton btnBuscar;
 	public JButton btnRegistrar;
 	private JTable table;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
-	public JRadioButton rbtdebito;
-	public JRadioButton rdbcredito;
-	public JRadioButton rdbtnEfectivo;
+	public JRadioButton rbtCredito;
+	public JRadioButton rdbDebito;
+	public JRadioButton rdbEfectivo;
 
 	/**
 	 * Launch the application.
@@ -63,7 +62,7 @@ public class VistaPagoAdministracion extends JFrame {
 	public VistaPagoAdministracion() {
 		setTitle("PAGO DE CUOTA ADMINISTRACION");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 1021, 453);
+		setBounds(100, 100, 924, 453);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(204, 204, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -73,7 +72,7 @@ public class VistaPagoAdministracion extends JFrame {
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(153, 204, 255));
 		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Datos del Residente", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 51, 0)));
-		panel.setBounds(10, 11, 965, 178);
+		panel.setBounds(10, 11, 891, 178);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -91,14 +90,13 @@ public class VistaPagoAdministracion extends JFrame {
 		txtCedula.setColumns(10);
 		
 		txtNombre = new JTextField();
-		txtNombre.setEditable(false);
 		txtNombre.setBounds(67, 66, 136, 20);
 		panel.add(txtNombre);
 		txtNombre.setColumns(10);
 		
 		btnBuscar = new JButton("buscar");
 		btnBuscar.setIcon(new ImageIcon(VistaPagoAdministracion.class.getResource("/iconos/Buscar32.png")));
-		btnBuscar.setBounds(235, 51, 136, 50);
+		btnBuscar.setBounds(222, 111, 136, 50);
 		panel.add(btnBuscar);
 		
 		JLabel Total = new JLabel("Total:");
@@ -106,35 +104,55 @@ public class VistaPagoAdministracion extends JFrame {
 		panel.add(Total);
 		
 		txtTotal = new JTextField();
-		txtTotal.setEditable(false);
 		txtTotal.setBounds(67, 108, 136, 20);
 		panel.add(txtTotal);
 		txtTotal.setColumns(10);
 		
 		btnRegistrar = new JButton("registrar");
-		btnRegistrar.setBounds(232, 117, 139, 50);
+		btnRegistrar.setBounds(368, 111, 139, 50);
 		panel.add(btnRegistrar);
 		btnRegistrar.setIcon(new ImageIcon(VistaPagoAdministracion.class.getResource("/iconos/Registrar.png")));
 		
-		rbtdebito = new JRadioButton("credito");
-		buttonGroup.add(rbtdebito);
-		rbtdebito.setBounds(235, 22, 65, 23);
-		panel.add(rbtdebito);
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(VistaPagoAdministracion.class.getResource("/iconos/Hombre64.png")));
+		lblNewLabel.setBounds(629, 19, 72, 64);
+		panel.add(lblNewLabel);
 		
-		rdbcredito = new JRadioButton("debito");
-		buttonGroup.add(rdbcredito);
-		rdbcredito.setBounds(302, 22, 55, 23);
-		panel.add(rdbcredito);
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(VistaPagoAdministracion.class.getResource("/iconos/pagos.png")));
+		lblNewLabel_1.setBounds(711, 26, 65, 57);
+		panel.add(lblNewLabel_1);
 		
-		rdbtnEfectivo = new JRadioButton("efectivo");
-		buttonGroup.add(rdbtnEfectivo);
-		rdbtnEfectivo.setBounds(359, 22, 65, 23);
-		panel.add(rdbtnEfectivo);
+		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(new ImageIcon(VistaPagoAdministracion.class.getResource("/iconos/Persona48.png")));
+		lblNewLabel_2.setBounds(791, 26, 65, 60);
+		panel.add(lblNewLabel_2);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new TitledBorder(null, "Seleccione medio de pago:", TitledBorder.CENTER, TitledBorder.TOP, null, Color.BLUE));
+		panel_2.setBounds(222, 19, 285, 50);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+		
+		rdbEfectivo = new JRadioButton("efectivo");
+		rdbEfectivo.setBounds(19, 21, 82, 23);
+		panel_2.add(rdbEfectivo);
+		buttonGroup.add(rdbEfectivo);
+		
+		rdbDebito = new JRadioButton("debito");
+		rdbDebito.setBounds(118, 21, 83, 23);
+		panel_2.add(rdbDebito);
+		buttonGroup.add(rdbDebito);
+		
+		rbtCredito = new JRadioButton("credito");
+		rbtCredito.setBounds(203, 21, 76, 23);
+		panel_2.add(rbtCredito);
+		buttonGroup.add(rbtCredito);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(new Color(175, 238, 238));
 		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Mostrar Pagos", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 255)));
-		panel_1.setBounds(10, 200, 965, 203);
+		panel_1.setBounds(10, 200, 891, 203);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
 		
@@ -142,26 +160,24 @@ public class VistaPagoAdministracion extends JFrame {
 		scrollPane.setViewportBorder(new LineBorder(new Color(0, 0, 0)));
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-		scrollPane.setBounds(10, 29, 709, 163);
+		scrollPane.setBounds(10, 29, 651, 163);
 		panel_1.add(scrollPane);
 		
 		table = new JTable();
-		tbpersonas=new DefaultTableModel(
+		table.setModel(new DefaultTableModel(
 			new Object[][] {
 				{null, null, null, null, "", null},
 			},
 			new String[] {
 				"Cedula", "Nombre", "Total", "ultimo pago", "Estado", "observaciones"
 			}
-		);
-		table.setModel(tbpersonas);
+		));
+		table.getColumnModel().getColumn(5).setPreferredWidth(109);
 		scrollPane.setViewportView(table);
 		
 		JButton btnNewButton = new JButton("mostrar pagos");
 		btnNewButton.setIcon(new ImageIcon(VistaPagoAdministracion.class.getResource("/iconos/Pago.png")));
-		btnNewButton.setBounds(770, 135, 185, 57);
+		btnNewButton.setBounds(673, 135, 185, 57);
 		panel_1.add(btnNewButton);
 	}
-	
-	
 }
